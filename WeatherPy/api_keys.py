7ebf2fe8160d67905271ab1c8596f22948e58bc6
micale24 <kaihall.usaf@gmail.com>
2020-08-25 21:49:01 -0400

    Added Weather API and successful code for request

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "8b496e3a35d0040aa8bd5651fcbdbb52"
+
+# Google API Key
+g_key = "AIzaSyCkZ2auJdtXQD3UKAsX17P4KxOwIjBOYC4"
---
+++ api_keys.py
@@ -0,0 +1,3 @@
+#Weather api key
+
+weather_api_key = "8b496e3a35d0040aa8bd5651fcbdbb52"
