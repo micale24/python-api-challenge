7a7d4382c8d8c1c9ecc7acc16d7f311478d7e355
micale24 <66092880+micale24@users.noreply.github.com>
2020-09-07 13:24:41 -0400

    omitting the api keys

--- VacationPy/api_key.py
+++ VacationPy/api_key.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "8b496e3a35d0040aa8bd5651fcbdbb52"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDOGb39QDoeOcOcdJ7BZAGSfDCPwahDBko"
+g_key = ""
